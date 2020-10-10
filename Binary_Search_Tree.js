462921afde263d10851fd13f4fa44f7683d07d61
K <gemigpost@hotmail.com>
2020-10-10 21:10:53 +0200

    BST class started

---
+++ Binary_Search_Tree.js
@@ -0,0 +1,51 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(val) {
+    const newNode = new Node(val);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (val === current.val) return undefined;
+        if (val < current.val) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (val > current.val) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+}
+
+const BST = new BinarySearchTree();
+BST.insert(10);
+BST.insert(10);
+BST.insert(1101);
+BST.insert(5);
+BST.insert(13);
+BST.insert(16);
+BST.insert(11);
+BST.insert(11);
+console.log(BST);
