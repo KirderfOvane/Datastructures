aa3090f64c41c02fd15e840e7f12f0d6a801f376
K <gemigpost@hotmail.com>
2020-10-17 00:55:39 +0200

    extractMax

--- MaxBinaryHeap.js
+++ MaxBinaryHeap.js
@@ -20,10 +20,69 @@ class MaxBinaryHeap {
     this.bubbleUp();
     return this.heap;
   }
+  extractMax() {
+    //swap root with last index
+    let temp = this.heap[0];
+    this.heap[0] = this.heap[this.heap.length - 1];
+    this.heap[this.heap.length - 1] = temp;
+    //remove last index from array (pop), store removed
+    const removed = this.heap.pop();
+    //sort root value
+    //compare with both child nodes,
+    let currentIndex = 0;
+    let leftChildIndex;
+    let rightChildIndex;
+    for (let i = 0; i < 10; i++) {
+      leftChildIndex = currentIndex * 2 + 1;
+      rightChildIndex = currentIndex * 2 + 2;
+      //if children is smaller or null/leaf your finished
+      let leftChild = this.heap[leftChildIndex];
+      let rightChild = this.heap[rightChildIndex];
+      //       console.log(leftChild,rightChild,this.heap[currentIndex])
+      //if both is larger than root value
+      //compare child nodes, largest is swapped with root
+      if (leftChild > rightChild) {
+        if (!leftChild) break;
+        if (leftChild > this.heap[currentIndex]) {
+          // swap leftChild and currentindexnode
+          temp = this.heap[currentIndex];
+
+          this.heap[currentIndex] = leftChild;
+          this.heap[leftChildIndex] = temp;
+          //console.log(leftChild,this.heap[currentIndex]);
+          // update current node to the swapped
+          currentIndex = leftChildIndex;
+          //            console.log(currentIndex);
+        }
+      } else {
+        if (!rightChild) break;
+        if (rightChild > this.heap[currentIndex]) {
+          // swap rightchild and currentindex
+          temp = this.heap[currentIndex];
+          this.heap[currentIndex] = rightChild;
+          this.heap[rightChildIndex] = temp;
+          // update current node to the swapped
+          currentIndex = rightChildIndex;
+        }
+      }
+      //    console.log(this.heap);
+    }
+    // when finished return removed
+    console.log(removed);
+    return;
+  }
 }
 const MyMaxBinaryHeap = new MaxBinaryHeap();
 const test4 = MyMaxBinaryHeap.insert(55);
 const test3 = MyMaxBinaryHeap.insert(1);
 const test2 = MyMaxBinaryHeap.insert(45);
 const test = MyMaxBinaryHeap.insert(199);
-console.log(test);
+// console.log(test);
+MyMaxBinaryHeap.extractMax();
+MyMaxBinaryHeap.extractMax();
+MyMaxBinaryHeap.extractMax();
+MyMaxBinaryHeap.extractMax();
+MyMaxBinaryHeap.extractMax();
+MyMaxBinaryHeap.extractMax();
+MyMaxBinaryHeap.extractMax();
+MyMaxBinaryHeap.extractMax();
