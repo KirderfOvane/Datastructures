2b9c10fa1eea66c7a034148c948cdaf2c0edc635
K <gemigpost@hotmail.com>
2020-10-22 21:14:02 +0200

    DynamicProgrammingExamples

---
+++ Dynamic_Programming_Examples.js
@@ -0,0 +1,27 @@
+// non memoized fibonacci sequence
+function fib(n, memo = []) {
+  if (memo[n] !== undefined) return memo[n];
+  if (n <= 2) return 1;
+  var res = fib(n - 1, memo) + fib(n - 2, memo);
+  memo[n] = res;
+  return res;
+}
+
+// memoized version
+function fib(n, memo = []) {
+  if (memo[n] !== undefined) return memo[n];
+  if (n <= 2) return 1;
+  var res = fib(n - 1, memo) + fib(n - 2, memo);
+  memo[n] = res;
+  return res;
+}
+
+// tabulated version
+function fib(n) {
+  if (n <= 2) return 1;
+  var fibNums = [0, 1, 1];
+  for (let i = 3; i <= n; i++) {
+    fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+  }
+  return fibNums[n];
+}
